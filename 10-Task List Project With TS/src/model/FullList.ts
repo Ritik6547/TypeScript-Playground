6f38096794f1c82a7d905e41e1b50f1b22904bae
Ritik <alphaop6547@gmail.com>
2025-09-16 01:47:08 +0530

    build task list project using TS

---
+++ 10-Task List Project With TS/src/model/FullList.ts
@@ -0,0 +1,60 @@
+import ListItem from "./ListItem";
+
+interface List {
+  list: ListItem[];
+  load(): void;
+  save(): void;
+  clearList(): void;
+  addItem(itemObj: ListItem): void;
+  removeItem(itemId: string): void;
+}
+
+export default class FullList implements List {
+  private _list: ListItem[];
+
+  static instance: FullList = new FullList();
+  private constructor() {
+    this._list = [];
+  }
+
+  get list(): ListItem[] {
+    return this._list;
+  }
+
+  load(): void {
+    const storedList: string | null = localStorage.getItem("myList");
+
+    if (typeof storedList !== "string") return;
+
+    const parsedList: { _id: string; _item: string; _checked: boolean }[] =
+      JSON.parse(storedList);
+
+    parsedList.forEach((itemObj) => {
+      const newListItem = new ListItem(
+        itemObj._id,
+        itemObj._item,
+        itemObj._checked
+      );
+      FullList.instance.addItem(newListItem);
+    });
+  }
+
+  save(): void {
+    localStorage.setItem("myList", JSON.stringify(this._list));
+  }
+
+  clearList(): void {
+    this._list = [];
+    this.save();
+  }
+
+  addItem(itemObj: ListItem): void {
+    this._list.push(itemObj);
+    this.save();
+  }
+
+  removeItem(itemId: string): void {
+    this._list = this._list.filter((item) => item.id !== itemId);
+    this.save();
+  }
+}
